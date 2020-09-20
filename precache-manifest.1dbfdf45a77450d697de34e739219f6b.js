5b1eb327fcef99bb3011344c7bf940c4fde6784a
Paola Garay <garay.angela@gmail.com>
2020-09-20 09:38:44 +0200

    Updates

---
+++ precache-manifest.1dbfdf45a77450d697de34e739219f6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf059f9f2aa2a9a1d57768862c92318",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "2fba407e6a7ed68534a0",
+    "url": "/client/static/js/2.1dc3fb3c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/client/static/js/2.1dc3fb3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08883df96b030447d64c",
+    "url": "/client/static/js/main.b4428256.chunk.js"
+  },
+  {
+    "revision": "80f9a0dd62ab6bdbaa51",
+    "url": "/client/static/js/runtime-main.4a683a20.js"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/client/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/client/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/client/static/media/twitter.435fc4bc.svg"
+  }
+]);
